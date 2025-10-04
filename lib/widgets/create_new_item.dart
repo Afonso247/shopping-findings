3c2234b8f83977faa4801b6141c2e325c8357a78
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-10-04 12:29:14 -0300

    feat: adicionando campos no form

--- lib/widgets/create_new_item.dart
+++ lib/widgets/create_new_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:findings/data/categories.dart';
+
 class CreateNewItem extends StatefulWidget {
   const CreateNewItem({super.key});
 
@@ -12,7 +14,55 @@ class _CreateNewItemState extends State<CreateNewItem> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8),
-      child: Text('Formulário'),
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              maxLength: 50,
+              decoration: const InputDecoration(label: Text('Nome')),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Quantidade'),
+                    ),
+                    keyboardType: TextInputType.number,
+                    initialValue: '1',
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: DropdownButtonFormField(
+                    items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 6),
+                              Text(category.value.title),
+                            ],
+                          ),
+                        ),
+                    ],
+                    onChanged: (value) {
+                      // Your code here
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
