4e10f6ef05ed2b619b33c77ec7fb76d485385b65
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-10-04 13:55:44 -0300

    feat: add texto caso nao hajam dados na lista

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Groceries',
       theme: ThemeData.dark().copyWith(
-        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(255, 147, 229, 250),
           brightness: Brightness.dark,
--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 
 class GroceryList extends StatelessWidget {
   const GroceryList({super.key, required this.groceryItems});
-
+  
   final List groceryItems;
-
+  
   @override
   Widget build(BuildContext context) {
+    if (groceryItems.isEmpty) {
+      return const Center(
+        child: Text(
+          'Nenhum item na lista.\nAdicione seus produtos!',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        ),
+      );
+    }
+    
     return ListView.builder(
       itemCount: groceryItems.length,
       itemBuilder: (context, index) => ListTile(
