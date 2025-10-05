7c68a9eacbf5cf318bda43a06bfd60e91b63cb2e
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-10-04 23:35:43 -0300

    fix: error handling de response.body == 'null'

--- lib/screens/grocery.dart
+++ lib/screens/grocery.dart
@@ -41,6 +41,13 @@ class _GroceryState extends State<Grocery> {
       return;
     }
 
+    if (response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
 
